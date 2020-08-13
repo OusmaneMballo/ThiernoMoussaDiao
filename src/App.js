61e5916551925604bf6164244e836b5a0f211692
OusmaneMballo <ousmanemballo576@gmail.com>
2020-08-13 02:17:14 +0000

    Interface de login et de sigin en react js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="login-wrap">
+        <div className="login-html">
+          <input id="tab-1" type="radio" name="tab" className="sign-in" checked />
+          <label for="tab-1" className="tab">Sign In</label>
+          <input id="tab-2" type="radio" name="tab" className="sign-up" />
+          <label for="tab-2" className="tab">Sign Up</label>
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label for="user" className="label">Username</label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">Password</label>
+                <input id="pass" type="password" className="input" data-type="password" />
+              </div>
+              <div className="group">
+                <input id="check" type="checkbox" className="check" checked />
+                <label for="check"><span className="icon"></span> Keep me Signed in</label>
+              </div>
+              <div class="group">
+                <input type="submit" className="button" value="Sign In" />
+              </div>
+              <div className="hr"></div>
+              <div className="foot-lnk">
+                <a href="#forgot">Forgot Password?</a>
+              </div>
+            </div>
+            <div className="sign-up-htm">
+              <div className="group">
+                <label for="user" className="label">Username</label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">Password</label>
+                <input id="pass" type="password" className="input" data-type="password" />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">Repeat Password</label>
+                <input id="pass" type="password" className="input" data-type="password" />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">Email Address</label>
+                <input id="pass" type="text" className="input" />
+              </div>
+              <div className="group">
+                <input type="submit" className="button" value="Sign Up" />
+              </div>
+              <div className="hr"></div>
+              <div className="foot-lnk">
+                <label for="tab-1"><a href="#">Already Member?</a></label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
